72983e8b1675032757216d7f053baef469fa1a15
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-18 14:41:45 +0200

    add puppet group + convenient binary  runpuppet

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,18 @@
+class puppet::basics{
+  file{"/usr/local/bin/runpuppet":
+    content => "sudo puppet apply --debug --verbose --summarize --reports store,riemann \
+      --modulepath '/tmp/vagrant-puppet/modules-0' \
+      /tmp/vagrant-puppet/manifests/site.pp --detailed-exitcodes",
+    mode => 0755, owner => root, group => root
+  }
+  group{"puppet":
+    ensure => present
+  }
+}
+
+
 node default {
+  include puppet::basics
   include riemann
   include riemann::dash::sample
   class { 'riemann::dash': config_file => '/etc/riemann-dash.rb' }
